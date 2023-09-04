8a157d16d3ff2b7d059cd918667cfb33dcad8044
Rivenshell Salomon <rivenshell111@gmail.com>
2023-09-04 04:18:58 -0400

    riv was here

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,13 @@ export default function Home() {
       </div>
       <div className=" content-center ">
         <SwitchColors />
+        <video className="h-full w-full rounded-lg" controls autoPlay>
+          <source
+            src="https://player.vimeo.com/video/843809307?h=4e0a7905c4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </>
   )
