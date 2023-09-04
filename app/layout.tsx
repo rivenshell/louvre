aade3e0f5c65d67a8c674732761649f3f6db0b5c
Rivenshell Salomon <rivenshell111@gmail.com>
2023-09-04 02:59:41 -0400

    riv was here

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Louvre",
+  title: "Louvre ",
   description: "Semi-static Portfolio Site",
 }
 
